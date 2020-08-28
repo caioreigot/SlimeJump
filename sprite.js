51cefd46e1495952e6ba49ea2522328652ffe2e0
Caio Costa <caiocrrodrigues2004@gmail.com>
2020-08-28 16:49:29 -0300

    Added new jump buttons
    Code has been rewritten in English, added new jump buttons (space and w).

--- sprite.js
+++ sprite.js
@@ -1,11 +1,11 @@
-function Sprite(x, y, largura, altura) {
+function Sprite(x, y, width, height) {
     this.x = x;
     this.y = y;
-    this.largura = largura;
-    this.altura = altura;
+    this.width = width;
+    this.height = height;
     
-    this.desenhar = function(xCanvas, yCanvas) {
-        ctx.drawImage(img, this.x, this.y, this.largura, this.altura, xCanvas, yCanvas, this.largura, this.altura);
+    this.draw = function(xCanvas, yCanvas) {
+        ctx.drawImage(img, this.x, this.y, this.width, this.height, xCanvas, yCanvas, this.width, this.height);
     }
 }
 
@@ -15,15 +15,15 @@ var bg = new Sprite(0, 0, 1600, 1600);
 spriteSlime = new Sprite(1859, 41, 50, 50);
 spriteSlimeMagma = new Sprite(1787, 41, 50, 50);
 
-// Chão
-spriteChao = new Sprite(0, 1601, 1600, 210);
+// Floor
+floorSprite = new Sprite(0, 1601, 1600, 210);
 
 // "Menus"
-perdeu = new Sprite(1627, 504, 343, 533);
-jogar = new Sprite(1658, 157, 298, 330);
-novo = new Sprite(1647, 1191, 261, 61);
+lose = new Sprite(1627, 504, 343, 533);
+play = new Sprite(1658, 157, 298, 330);
+newBoard = new Sprite(1647, 1191, 261, 61);
 spriteRecord = new Sprite(1647, 1088, 330, 73);
-spritePlacar = new Sprite(1609, 1277, 320, 170);
+scoreboard = new Sprite(1609, 1277, 320, 170);
 
 // Imagem dos obstáculos
 magmaObstacle1 = new Sprite(1623, 1466, 50, 120);
