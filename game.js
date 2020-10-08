f393c5b3db9e4667a015379426e487c10bbfb126
Caio Costa <62410044+caioreigot@users.noreply.github.com>
2020-10-08 19:58:57 -0300

    Update game.js

--- game.js
+++ game.js
@@ -187,8 +187,8 @@ function main() {
         screenHeight = 600;
     }
     // Maximum height
-    if (screenHeight >= 700) {
-        screenHeight = 700;
+    if (screenHeight >= 900) {
+        screenHeight = 900;
     }
 
     // Setting the maximum width
